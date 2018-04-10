e743eceb948c8143659278c4f3cc9d8c708fa177
Chris Bowman <chbowm@microsoft.com>
2018-04-10 14:10:58 +1000

    Added Get-SQLServerIPs.ps1

---
+++ Get-SQLServerIPs.ps1
@@ -0,0 +1,15 @@
+﻿if((Get-AzureRmContext).Account -eq $null) {
+    Write-Host "Logging into AzureRM"
+    Login-AzureRmAccount
+}
+
+$subs=Get-AzureRmSubscription | Out-GridView -PassThru -Title "Subscription selection"
+
+foreach ($sub in $subs) {
+    $sub | Select-AzureRmSubscription | Out-Null
+    Write-Host "working on $($sub.Name) $($sub.Id)"
+    $servers = Get-AzureRmSqlServer | Where-Object {$_.Location -eq "australiasoutheast"}
+    foreach ($server in $servers) {
+        Write-Host "$($server.ServerName): $((Resolve-DnsName $server.FullyQualifiedDomainName).IPAddress)"
+    }
+} 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # random-stuff
+A bunch of random stuff I've come up with
+
+# Get-SQLServerIPs.ps1
+Script used to check IPs of Azure SQL Server endpoints 
